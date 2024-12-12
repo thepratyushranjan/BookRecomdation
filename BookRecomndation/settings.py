5d0d825504715f14d66a54efe684bef4ef3d52f7
pratyush.ranjan <thepratyushranjan6060@gmail.com>
2024-12-13 01:59:29 +0530

    Update

--- BookRecomndation/settings.py
+++ BookRecomndation/settings.py
@@ -68,14 +68,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'BookRecomndation.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'bookrecomndation',          
+#         'USER': 'root',         
+#         'PASSWORD': 'root', 
+#         'HOST': 'localhost',                  
+#         'PORT': '3306',                        
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bookrecomndation',          
-        'USER': 'root',         
-        'PASSWORD': 'root', 
-        'HOST': 'localhost',                  
-        'PORT': '3306',                        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
